bd9add28e59b9aa91a05498c3165320a8e617802
Yuri <51887884+JerzyXD@users.noreply.github.com>
2024-09-06 21:39:08 +0300

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "kzt_library"
+include(":app-ui")
+include(":app-data")
+include(":app-domain")
+include(":app")
